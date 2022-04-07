8f7d24edfb25f231a5ec4a04cfa275c19ff5edf8
Ochilova Alisa Aleksandrovna <a.ochilova@tinkoff.ru>
2022-04-07 18:48:35 +0300

    feat: тренировка #5.2-5.3, заменила конкатенацию подстрок на форматирование по шаблону, добавлено разделение страницы при выводе

--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -1,13 +1,21 @@
 package com.tcs.edu.decorator;
 
-import  java.time.Instant;
+import java.time.Instant;
+import static java.lang.String.format;
 
 public class TimestampMessageDecorator {
 
-    public static int messageCount;
+    private static int messageCount;
 
     public static String decorator(String message){
-        final var decoratedMessage = ++messageCount + " " + Instant.now() + " " + message;
-        return decoratedMessage;
+
+        if (++messageCount % 2 == 0 ) {
+            final String decoratedMessage = format("%d %s %s %n---",messageCount,  Instant.now().toString(), message);
+            return decoratedMessage;
+        }
+        else {
+            final String decoratedMessage = format("%d %s %s",messageCount,  Instant.now().toString(), message);
+            return decoratedMessage;
+        }
     }
 }
--- src/test/java/com/tinkoff/edu/Application.java
+++ src/test/java/com/tinkoff/edu/Application.java
@@ -1,10 +1,13 @@
-package com.tcs.edu;
+package com.tinkoff.edu;
 
 import static com.tcs.edu.decorator.TimestampMessageDecorator.*;
 import static com.tcs.edu.printer.ConsolePrinter.print;
 
 class Application {
     public static void main(String[] args) {
+        print(decorator("Hello world!"));
+        print(decorator("Hello world!"));
+        print(decorator("Hello world!"));
         print(decorator("Hello world!"));
         print(decorator("Hello world!"));
         print(decorator("Hello world!"));
