43bdc447ad06ec67ef196daaa043304a691dc4ea
Ochilova Alisa Aleksandrovna <a.ochilova@tinkoff.ru>
2022-05-16 14:08:58 +0300

    feat: тренировка 09-1/09-2:  поправила     isArgsValid

--- src/main/java/com/tcs/edu/decorator/ValidatedService.java
+++ src/main/java/com/tcs/edu/decorator/ValidatedService.java
@@ -6,10 +6,10 @@ import com.tcs.edu.enums.MessageOrder;
 
 public abstract class ValidatedService {
     public boolean isArgsValid(MessageOrder order, Doubling doubling, Message message) {
-        return message.getBody() != null && message.getLevel() != null && message != null && order != null && doubling != null;
+        return isArgsValid(order, message) && doubling != null;
     }
     public boolean isArgsValid(MessageOrder order,  Message message) {
-        return message.getBody() != null && message.getLevel() != null && message != null && order != null;
+        return isArgsValid(message) && order != null;
     }
     public boolean isArgsValid(Message message) {
         return message.getBody() != null && message.getLevel() != null && message != null;
