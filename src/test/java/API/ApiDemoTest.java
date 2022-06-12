775804f177a93dc54f2707afb10a482c3a4f018d
Ochilova Alisa Aleksandrovna <a.ochilova@tinkoff.ru>
2022-06-12 22:29:48 +0300

    feat: Тренировка #13 - финал

--- src/test/java/API/ApiDemoTest.java
+++ src/test/java/API/ApiDemoTest.java
@@ -1,16 +1,25 @@
 package API;
 
-import API.Dto.CountryPojo;
 import io.restassured.RestAssured;
 import io.restassured.authentication.PreemptiveBasicAuthScheme;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.hamcrest.Matchers.*;
 
-public class ApiDemoTest extends CountryPojo {
+public class ApiDemoTest {
+    String countryName = randomAlphabetic(2);
+
+    Random random = new Random();
+    int id = random.nextInt(1000);
+    String idString = String.valueOf(id);
+
+
     @BeforeAll
     public static void setUpAuth() {
         PreemptiveBasicAuthScheme authScheme = new PreemptiveBasicAuthScheme();
@@ -25,68 +34,91 @@ public class ApiDemoTest extends CountryPojo {
     }
 
     @Test
-    public void shouldGetCountriesDb() {
-        when()
-                .get("/api/countries")
+    public void shouldCreateCountriesDb() {
+        given()
+                .contentType("application/json")
+                .body("{\n" +
+                        "  \"countryName\": \"" + countryName + "\"\n" +
+                        "}")
+                .when()
+                .post("/api/countries")
                 .then()
-                .statusCode(200)
-                .body(
-                        "size()", is(12),
-                        "[0].countryName", is("vi")
-                );
+                .statusCode(201)
+                .body("id", not(empty()),
+                        "countryName", is(countryName),
+                        "location", nullValue())
+                .extract()
+                .response()
+                .path(idString = "id");
+        when().delete("/api/countries/" + id);
     }
 
     @Test
     public void shouldGetCountry() {
+        id = given()
+                .body("{\n" +
+                        "  \"countryName\": \"" + countryName + "\"\n" +
+                        "}")
+                .contentType("application/json")
+                .when()
+                .post("/api/countries")
+                .body()
+                .path("id");
         when()
-                .get("/api/countries/3")
+                .get("/api/countries/" + id)
                 .then()
                 .statusCode(200)
                 .body(
-                        "id", is(3),
-                        "countryName", is("St"),
+                        "id", is(id),
+                        "countryName", is(countryName),
                         "location", nullValue()
                 );
-    }
-
-    @Test
-    public void shouldCreateCountryWhenUniqu() {
-        given()
-                .contentType("application/json")
-                .body("{\n" +
-                        "  \"countryName\": \"CT\"\n" +
-                        "}")
-                .when()
-                .post("/api/countries/")
-                .then()
-                .statusCode(201)
-                .body("id", not(empty()),
-                        "countryName", is("CT"),
-                        "location", nullValue());
+        when().delete("/api/countries/" + id);
     }
 
     @Test
     public void shouldDeleteCountry() {
+        id = given()
+                .body("{\n" +
+                        "  \"countryName\": \"" + countryName + "\"\n" +
+                        "}")
+                .contentType("application/json")
+                .when()
+                .post("/api/countries")
+                .body()
+                .path("id");
         when()
-                .delete("/api/countries/" + getId())
+                .delete("/api/countries/" + id)
                 .then()
                 .statusCode(204);
     }
 
     @Test
     public void shouldPatchCountry() {
+        id = given()
+                .body("{\n" +
+                        "  \"countryName\": \"" + countryName + "\"\n" +
+                        "}")
+                .contentType("application/json")
+                .when()
+                .post("/api/countries")
+                .body()
+                .path("id");
         given()
                 .contentType("application/json")
-                .body("{\n" +
-                        "  \"id\": 10,\n" +
-                        "  \"countryName\": \"de\"\n" +
-                        "}")
+                .body(
+                        "{\n" +
+                                "  \"id\": \"" + id + "\"\n" +
+                                "," +
+                                "  \"countryName\": \"" + countryName + "\"\n" +
+                                "}")
                 .when()
-                .patch("/api/countries/10")
+                .patch("/api/countries/" + id)
                 .then()
                 .statusCode(200)
-                .body("id", not(empty()),
-                        "countryName", is("de"),
+                .body("id", is(id),
+                        "countryName", is(countryName),
                         "location", nullValue());
+        when().delete("/api/countries/" + id);
     }
 }
